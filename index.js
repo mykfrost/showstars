202c0ed82f845aed3f8bcb33d378b1260fb46e06
Michael Okello <okellofrost@gmail.com>
2022-07-10 13:09:28 +0300

    Add braces

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ function showStars(rows){
 
     for(let row = 1; row <= rows; row++){
         let pattern = '';
-        for(let i = 0 ; i < row; i++){
-            pattern += '*';
-            console.log(pattern);
-        }
-    }
-
-
+     for(let i = 0 ; i < row ; i++)
+     pattern += '*';
+    console.log(pattern);
+}
 }
